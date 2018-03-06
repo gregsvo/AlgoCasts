c295363a821df8c43cfafc8d58dc36b3952d4ebe
Greg Svoboda <gregsvo@gmail.com>
2018-03-05 19:19:46 -0500

    ReverseString Practice

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+
+    // Solution One:
+    // return str
+    //     .split('')
+    //     .reverse()
+    //     .join('');
+
+    // Solution Two:
+    // let text = '';
+    // debugger;
+    // for (let i = str.split('').length - 1; i >= 0; i--) {
+    //     text += str[i];
+    // }
+    // return text;
+
+    // Solution Three:
+    // let reversed = '';
+    // for (let character of str){
+    //     reversed = character + reversed;
+    // }
+    // return reversed;
+
+    // Solution Four:
+    // return str.split('').reduce((reversed, character) => {
+    //     return character + reversed;
+    // }, '');
+
+    // Solution Five:
+    // return str.split('').reduce((reversed, character) => character + reversed, '');
+}
+
+reverse('svoboda');
 
 module.exports = reverse;
